cb34b88e3881f1ff396dc15b1f0435502799300b
claudiovb <claudiocarvalhovilasboas@gmail.com>
2025-08-13 21:48:45 +0200

    FEAT: remove wrong merge cherry-pick

--- bare.js
+++ bare.js
@@ -18,3 +18,4 @@ import 'bare-wdk-runtime'
 export * from './index.js' with { imports: 'bare-wdk-runtime/package' }
 
 export { default } from './index.js' with { imports: 'bare-wdk-runtime/package' }
+
